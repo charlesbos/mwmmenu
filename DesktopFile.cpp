6ebec665dca5922c417c51241a3a47e5011f94c2
Charles Bos <charlesbos1@gmail.com>
2015-11-20 11:18:01 +0000

    add copyright notices

--- DesktopFile.cpp
+++ DesktopFile.cpp
@@ -1,3 +1,23 @@
+/*
+mwmmenu - a program to produce menus for the Motif Window Manager, based on
+freedesktop.org desktop entries,
+
+Copyright (C) 2015  Charles Bos
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <algorithm>
 #include <set>
 #include <string.h>
